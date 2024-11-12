230b299d0a24cfba48f46561917b73dc13029c22
Carlos1681 <spjcarlosh@gmail.com>
2024-11-12 11:41:50 -0300

    Database seed update

--- gamelist/src/main/java/com/games/gamelist/model/GameList.java
+++ gamelist/src/main/java/com/games/gamelist/model/GameList.java
@@ -3,12 +3,17 @@ package com.games.gamelist.model;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="tb_game_list")
 public class GameList {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String Name;
 	
